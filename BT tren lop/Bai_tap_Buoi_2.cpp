74cb606245036e67204b857678cd543708c493bc
Tran Quang Vu <minion2007vu@gmail.com>
2026-09-17 10:05:45 +0700

    ham random tham khao AI, viet not cac ham trong class User

--- BT tren lop/Bai_tap_Buoi_2.cpp
+++ BT tren lop/Bai_tap_Buoi_2.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <random>
 
 using namespace std;
  
@@ -26,7 +27,6 @@ class User{
         string username;
         string DOB;
         string region;
-        int listen_time;
         
         vector<Song> liked_list;
         unordered_map<string, Song> liked_map;
@@ -36,11 +36,49 @@ class User{
 
         vector<Song> history_list;
         unordered_map<string, Song> history_map;
+        unordered_map<string, int> listen_times;
 
-        void listen_to_song(const Song& song){
-            history_map.insert({song.ID_song, song});
+        string generate_ID(){
+            random_device rd;  
+            mt19937 gen(rd()); 
+            uniform_int_distribution<> dis(10000000, 99999999);
+            return "User_" + to_string(dis(gen));
+        }
+        
+        void register_user(){
+            ID_user = generate_ID();
+            cout << "Create username: ";
+            cin >> username;
+            cout << endl;
+            cout << "Date of Birth: ";
+            cin >> DOB;
+            cout << endl;
+            cout << "Country: ";
+            cin >> region;
         }
 
+        void like_song(const Song& song){
+            if (liked_map.find(song.ID_song) == liked_map.end()) {
+                liked_list.push_back(song);
+                liked_map[song.ID_song] = song;
+            }
+            else liked_map.erase(song.ID_song);
+        }
+
+        void download_song(const Song& song){
+            if (downloaded_map.find(song.ID_song) == downloaded_map.end()) {
+                downloaded_list.push_back(song);
+                downloaded_map[song.ID_song] = song;
+            }
+        }
+
+        void listen_to_song(const Song& song){
+            if (history_map.find(song.ID_song) == history_map.end()) {
+                history_list.push_back(song);
+                history_map[song.ID_song] = song;
+            }
+            listen_times[song.ID_song]++;
+        }
 };
 
 vector<Song> mutual_likes(const User& user_1, const User& user_2){
@@ -86,7 +124,7 @@ void User_relation(const User& user_1, const User& user_2){
     vector<Song> mutual_likes_list = mutual_likes(user_1, user_2);
     vector<Song> mutual_downloaded_list = mutual_downloaded(user_1, user_2);
 
-    similarity_score = sizeof(mutual_downloaded_list)*5 + sizeof(mutual_likes_list)*3;
+    similarity_score = mutual_downloaded_list.size()*5 + mutual_likes_list.size()*3;
 
 };
 
