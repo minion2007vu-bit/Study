5d5998ed008812565ba873b74f1e0b82acb61139
Tran Vu <minion2007vu@gmail.com>
2026-09-25 08:09:03 +0700

    viet sap xong roi

--- BT tren lop/Bai_tap_Buoi_2(slide 27).cpp
+++ BT tren lop/Bai_tap_Buoi_2(slide 27).cpp
@@ -8,12 +8,45 @@
 using namespace std;
 
 struct Coin_solution{
-    int sum;
-    vector<int> coin_data;
-    int num_of_types;
+    int sum;                        //tong can tim
+    int num_of_types;               //so luong cac loai dong xu
+    int limit;                      //gioi han dai dien cho nhung tong chua co cach chia
+    vector<int> coin_data;          //gia tri tung dong xu
+    vector<int> coin_used;          //so luong dong xu de dc tong s
+    
+    Coin_solution(int sum, const vector<int>& coin_data, int num_of_types):
+        sum(sum),
+        num_of_types(num_of_types),
+        limit(sum + 1),
+        coin_data(coin_data),
+        coin_used(limit, -1) {}
 
-
-    vector<int> coin_used;
     vector<int> last_used;
+
+    void find_solution(){
+        for( int i = 0; i <= sum; i++){
+            for( int j = 0; j < num_of_types; j++){
+                if(coin_data[j] <= i && coin_used[sum- coin_data[j]] != limit){
+                    if(coin_used[sum - coin_data[j]] + 1 < coin_used[i]){
+                        coin_used[i] = coin_used[i - coin_data[j]] + 1;
+                        last_used[i] = j;
+                    }
+                }
+            }
+        }
+    }
+
+    void output(){
+        if(coin_used[sum] == limit){
+            cout << "khong co cach chia nao thoa man tong S";
+            return;
+        }
+        else{
+            cout << "So xu toi thieu de duoc tong S la: " << coin_used[sum];
+            
+            return;
+        }
+    }
+
 };
 
